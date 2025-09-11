93533119f69f328574a6d66890fa61e2249860e3
Shuqin2025 <shuqinamberg@gmail.com>
2025-09-11 19:40:26 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
-// frontend/src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  // 如不需要严格模式，可去掉 <React.StrictMode>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 入口只做一件事：把 <App/> 挂到 #root
+const el = document.getElementById("root");
+if (!el) {
+  // 理论上不会发生；防御一下
+  const msg = "找不到 #root。请检查 index.html 中的 <div id=\"root\"></div>";
+  console.error(msg);
+  document.body.innerHTML = `<pre style="padding:16px;color:#c00">${msg}</pre>`;
+} else {
+  createRoot(el).render(<App />);
+}
